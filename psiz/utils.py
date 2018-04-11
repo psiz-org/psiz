9a3f5035efcacc17be483fea5bdf05f9550711c5
roads <brett.roads@gmail.com>
2018-04-11 14:20:10 +0100

    added function for determining possible outcomes of a display

--- psiz/utils.py
+++ psiz/utils.py
@@ -6,18 +6,59 @@ Author: B D Roads
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from itertools import permutations
 
 def matrix_correlation(mat_A, mat_B):
-    """Returns the R^2 score between two square matrices.
+    """Return the R^2 score between two square matrices.
 
-    Parameters:
-      mat_A: A square matrix.
-      mat_B: A square matrix the same size as mat_A
-      is_sym: Boolean indicating if the matrices are symmetric.
+    Args:
+        mat_A: A square matrix.
+        mat_B: A square matrix the same size as mat_A
+        is_sym: Boolean indicating if the matrices are symmetric.
+    
+    Returns:
+        The R^2 score between the two matrices.
     """
     n_row = mat_A.shape[0]
     iu1 = np.triu_indices(n_row,1)
 
     # Explained variance score.
     return r2_score(mat_A[iu1], mat_B[iu1])
+
+def possible_outcomes(display_configuration):
+    """Return the possible outcomes of a display configuration.
+
+    Args:
+        display_configuration: A display configuration Pandas Series.
+
+    Returns:
+        An array indicating all possible outcomes where the values
+            indicate indices of the reference stimuli.
+    """
+
+    n_reference = display_configuration['n_reference']
+    n_selected = int(display_configuration['n_selected'])
+
+    reference_list = range(n_reference)
+    
+    # Get all permutations of length n_selected.
+    perm = permutations(reference_list, n_selected)
+
+    selection = list(perm)
+    n_outcome = len(selection)
+    
+    outcomes = np.empty((n_outcome, n_reference), dtype=np.int32)
+    for i_outcome in range(n_outcome):
+        # Fill in selections.
+        outcomes[i_outcome,0:n_selected] = selection[i_outcome]
+        # Fill in unselected.
+        dummy_idx = np.arange(n_reference)
+        for i_selected in range(n_selected):
+            loc = dummy_idx != outcomes[i_outcome,i_selected]
+            dummy_idx = dummy_idx[loc]
+        
+        outcomes[i_outcome,n_selected:] = dummy_idx
+
+    return outcomes
+
 
---
+++ tests/test_utils.py
@@ -0,0 +1,72 @@
+"""Module for testing utils.py
+
+Todo:
+    group into classes
+"""
+
+import numpy as np
+
+from psiz.utils import possible_outcomes
+from psiz.models import Observations
+
+def test_possible_outcomes_2c1():
+    """
+    """
+    displays = np.array(((0, 1, 2), (9, 12, 7)))
+    n_selected = 1 * np.ones((2))
+    obs = Observations(displays, n_selected=n_selected)
+    
+    po = possible_outcomes(obs.configurations.iloc[0])
+
+    correct = np.array(((0, 1), (1, 0)))
+    np.testing.assert_array_equal(po, correct)
+
+def test_possible_outcomes_3c2():
+    """
+    """
+    displays = np.array(((0, 1, 2, 3), (33, 9, 12, 7)))
+    n_selected = 2 * np.ones((2))
+    obs = Observations(displays, n_selected=n_selected)
+
+    po = possible_outcomes(obs.configurations.iloc[0])
+
+    correct = np.array(((0, 1, 2), (0, 2, 1), (1, 0, 2), (1, 2, 0), 
+    (2, 0, 1), (2, 1, 0)))
+    np.testing.assert_array_equal(po, correct)
+
+def test_possible_outcomes_4c2():
+    """
+    """
+    # displays = np.array(((0, 1, 2, 3, 4, 5, 6, 7, 8), (9, 11, 13, 2, 6, 12, 7, 23, 9)))
+    displays = np.array(((0, 1, 2, 3, 4), (45, 33, 9, 12, 7)))
+    n_selected = 2 * np.ones((2))
+    obs = Observations(displays, n_selected=n_selected)
+
+    po = possible_outcomes(obs.configurations.iloc[0])
+
+    correct = np.array(((0, 1, 2, 3), (0, 2, 1, 3), (0, 3, 1, 2), 
+    (1, 0, 2, 3), (1, 2, 0, 3), (1, 3, 0, 2),
+    (2, 0, 1, 3), (2, 1, 0, 3), (2, 3, 0, 1),
+    (3, 0, 1, 2), (3, 1, 0, 2), (3, 2, 0, 1)))
+    np.testing.assert_array_equal(po, correct)
+
+def test_possible_outcomes_8c1():
+    """
+    """
+    # displays = np.array(((0, 1, 2, 3, 4, 5, 6, 7, 8), (9, 11, 13, 2, 6, 12, 7, 23, 9)))
+    displays = np.array(((0, 1, 2, 3, 4, 5, 6, 7, 8), 
+    (45, 33, 9, 12, 7, 2, 5, 4, 3)))
+    n_selected = 1 * np.ones((2))
+    obs = Observations(displays, n_selected=n_selected)
+
+    po = possible_outcomes(obs.configurations.iloc[0])
+
+    correct = np.array(((0, 1, 2, 3, 4, 5, 6, 7), 
+    (1, 0, 2, 3, 4, 5, 6, 7), 
+    (2, 0, 1, 3, 4, 5, 6, 7), 
+    (3, 0, 1, 2, 4, 5, 6, 7), 
+    (4, 0, 1, 2, 3, 5, 6, 7), 
+    (5, 0, 1, 2, 3, 4, 6, 7),
+    (6, 0, 1, 2, 3, 4, 5, 7), 
+    (7, 0, 1, 2, 3, 4, 5, 6)))
+    np.testing.assert_array_equal(po, correct)

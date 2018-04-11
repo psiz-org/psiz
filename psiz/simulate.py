d16e0b2ed6ef5147dd400d2abcd0afc79d33c9d7
roads <brett.roads@gmail.com>
2018-04-11 14:21:02 +0100

    started adding simulation functionality

--- psiz/simulate.py
+++ psiz/simulate.py
@@ -1,4 +1,36 @@
-"""Module for simulating human behavior.
+"""Module for simulating agent behavior.
 
 Author: B D Roads
 """
+
+class Agent(object):
+    """Agent that simulates similarity judgments.
+    """
+
+    def __init__(self, embedding, group_idx=0):
+        """Initialize.
+
+        Args:
+            embedding:
+            group_id (optional):
+        """
+
+        self.embedding = embedding
+        self.group_idx = group_idx
+    
+    def simulate(self, displays):
+        """Simulate similarity judgments for provided displays.
+
+        Args:
+            displays: Observations object representing the
+                to-be-judged displays. The order of the stimuli in the
+                stimulus set is ignored for the simulations.
+        
+        Returns:
+            Observations object representing the judged displays. The
+                order of the stimuli is now informative.
+        """
+
+    def _probability(self, displays):
+            """
+            """

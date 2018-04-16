d51ac5df23861525bdda431c91e25d043807ab40
roads <brett.roads@gmail.com>
2018-04-16 01:01:27 +0100

    adding functionality to simulate

--- psiz/simulate.py
+++ psiz/simulate.py
@@ -16,12 +16,23 @@
 
 """Module for simulating agent behavior.
 
+Classes:
+    Agent: An object that can be initialized using a psychological
+        embedding and used to simulate similarity judgments.
 """
 
 from psiz.trials import JudgedTrials
 
+
 class Agent(object):
     """Agent that simulates similarity judgments.
+
+    Attributes:
+        embedding:
+        group_idx:
+    Methods:
+        simulate:
+
     """
 
     def __init__(self, embedding, group_idx=0):
@@ -29,25 +40,49 @@ class Agent(object):
 
         Args:
             embedding:
-            group_id (optional):
+            group_id (optional): If the provided embedding was inferred
+                for more than one group, an index can be provided to
+                indicate which set of attention weights should be used.
         """
-
         self.embedding = embedding
         self.group_idx = group_idx
-    
-    def simulate(self, displays):
-        """Simulate similarity judgments for provided displays.
+
+    def simulate(self, trials):
+        """Simulate similarity judgments for provided trials.
 
         Args:
             displays: UnjudgedTrials object representing the
-                to-be-judged displays. The order of the stimuli in the
+                to-be-judged trials. The order of the stimuli in the
                 stimulus set is ignored for the simulations.
-        
-        Returns:
-            JudgedTrials object representing the judged displays. 
-                The order of the stimuli is now informative.
-        """
 
-    def _probability(self, displays):
-            """
-            """
+        Returns:
+            JudgedTrials object representing the judged trials. The
+                order of the stimuli is now informative.
+
+        """
+        probs = self._probability(trials)
+        return JudgedTrials(trials.stimulus_set)  # HACK
+
+    def _probability(self, trials):
+        """Return probability of outcomes for each trial.
+        
+        Args:
+            trials: A set of unjudged similarity trials.
+
+        Returns:
+            The probabilities associated with the different outcomes
+                for each unjudged trial.
+
+        """
+        trial_configurations = trials.configurations
+
+        Z_q = None
+        Z_ref = None
+
+        
+        outcome_idx = possible_outcomes(display_configuration)
+
+        # Precompute similarity between query and references.
+        self.embedding.similarity(Z_q, Z_ref)
+
+        # Compute probability of each possible outcome.
--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,6 +1,62 @@
-"""Module for testing simulate.py
+# -*- coding: utf-8 -*-
+# Copyright 2018 The PsiZ Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
-Todo:
+"""Module for testing simulate.py."""
 
-Author: B. D. Roads
-"""
+
+import numpy as np
+import pytest
+
+from psiz.trials import UnjudgedTrials
+from psiz.models import Exponential
+from psiz.simulate import Agent
+
+
+@pytest.fixture(scope="module")
+def ground_truth():
+    """Return a ground truth embedding."""
+    n_stimuli = 10
+    dimensionality = 2
+
+    model = Exponential(n_stimuli)
+    mean = np.ones((dimensionality))
+    cov = np.identity(dimensionality)
+    Z = np.random.multivariate_normal(mean, cov, (n_stimuli))
+    freeze_options = {
+        'rho': 2,
+        'tau': 1,
+        'beta': 1,
+        'gamma': 0,
+        'Z': Z
+    }
+    model.freeze(freeze_options)
+    return model
+
+
+@pytest.fixture(scope="module")
+def unjudged_trials():
+    """Return a set of unjudged trials."""
+    stimulus_set = np.array(((0, 1, 2),
+                            (3, 4, 5)))
+    unjudged_trials = UnjudgedTrials(stimulus_set)
+    return unjudged_trials
+
+
+def test_simulate(ground_truth, unjudged_trials):
+    """Test simulation of agent."""
+    agent = Agent(ground_truth)
+    obs = agent.simulate(unjudged_trials)
+    assert obs.n_trial is 2

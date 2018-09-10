ee3c03de7806023561f0844479edd78a3d0ab11b
roads <brett.roads@gmail.com>
2018-09-10 10:53:11 +0100

    Notes and code changes for visualize.py.

--- psiz/visualize.py
+++ psiz/visualize.py
@@ -14,7 +14,13 @@
 # limitations under the License.
 # ==============================================================================
 
-"""Module for visualizing embeddings."""
+"""Module for visualizing embeddings.
+
+Todo:
+    class_vec -> class_id
+    classes -> class_dict
+    filename -> fname
+"""
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
@@ -23,19 +29,19 @@ import numpy as np
 
 
 def visualize_embedding_static(
-        Z, class_vec=None, classes=None, special_locs=None, filename=None):
+        Z, class_vec=None, classes=None, filename=None):
     """Generate a static scatter plot of the supplied embedding points.
 
     Arguments:
-      Z: A real-valued two-dimensional array representing the embedding.
-        shape = (n_stimuli, n_dim)
-      class_vec: (optional) An integer array contianing class IDs that indicate
-        the class membership of each stimulus.
-        shape = (n_stimuli, 1)
-      classes: (optional) A dictionary mapping class IDs to strings.
-      special_locs: (optional) A boolean array indicating special points to
-        emphasize with a diamond. TODO
-        shape = (n_stimuli, 1)
+        Z: A real-valued two-dimensional array representing the embedding.
+            shape = (n_stimuli, n_dim)
+        class_vec: (optional) An integer array contianing class IDs
+            that indicate the class membership of each stimulus.
+            shape = (n_stimuli, 1)
+        classes: (optional) A dictionary mapping class IDs to strings.
+        filename (optional): The pdf filename to save the figure,
+            otherwise the figure is displayed.
+
     """
     # Settings
     dot_size = 20
@@ -112,6 +118,7 @@ def visualize_embedding_static(
         ax.set_zticks([], [])
 
     if filename is None:
+        # plt.tight_layout()
         plt.show()
     else:
         # Note: The dpi must be supplied otherwise the aspect ratio will be

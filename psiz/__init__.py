c274824f8a4c27ddec44405d1a0fc9022df5c673
roads <brett.roads@gmail.com>
2019-01-30 14:34:35 +0000

    Updated __init__.py to include __all__ statement.

--- psiz/__init__.py
+++ psiz/__init__.py
@@ -1,7 +1,16 @@
 """
 Modules:
-    models
+    datasets
     dimensionality
+    generator
+    models
+    simulate
+    trials
     utils
     visualize
 """
+
+__all__ = [
+    'datasets', 'dimensionality', 'generator', 'models', 'simulate',
+    'trials', 'utils', 'visualize'
+]

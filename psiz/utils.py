1497688d8add926e7f5b2672a598cbb9250d0c15
roads <brett.roads@gmail.com>
2019-08-08 08:00:04 +0100

    Minor formatting of seconds for progress bar output.

--- psiz/utils.py
+++ psiz/utils.py
@@ -466,5 +466,7 @@ class ProgressBar(object):
         if iteration == self.total:
             self._stop()
             print()
-            total_str = str(datetime.timedelta(seconds=self.total_s))
+            total_str = str(
+                datetime.timedelta(seconds=np.round(self.total_s))
+            )
             print('    Elapsed time: {0}'.format(total_str))

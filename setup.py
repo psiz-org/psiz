3a74f46b24fdbde3af5c6c8dd58e397bbf2b7ac4
roads <brett.roads@gmail.com>
2020-02-10 10:29:52 +0000

    Add missing 'pillow' package requirement.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     python_requires='>=3, <3.8',
     install_requires=[
         'tensorflow>=2', 'scipy', 'pandas', 'scikit-learn',
-        'matplotlib', 'tensorflow-probability', 'numexpr'
+        'matplotlib', 'tensorflow-probability', 'numexpr', 'pillow'
     ],
     include_package_data=True,
     url='https://github.com/roads/psiz',

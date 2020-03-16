f3ad7d12e21b3f8397019588b8a688060d74a4d6
roads <brett.roads@gmail.com>
2020-03-16 16:27:52 +0000

    Add experimental visualization functions.

--- psiz/visualize.py
+++ psiz/visualize.py
@@ -269,3 +269,141 @@ def infer_legend(unique_class_list, classes):
         class_label = classes[unique_class_list[i_class]]
         legend_entries.append(class_label)
     return legend_entries
+
+
+def dot_2d(
+        ax, z, class_id=None, classes=None):
+    """Generate a static scatter plot of the supplied embedding points.
+
+    Arguments:
+        z: A real-valued two-dimensional array representing the embedding.
+            shape = (n_stimuli, n_dim)
+        class_id: (optional) An integer array contianing class IDs
+            that indicate the class membership of each stimulus.
+            shape = (n_stimuli, 1)
+        classes: (optional) A dictionary mapping class IDs to strings.
+        fname (optional): The pdf filename to save the figure,
+            otherwise the figure is displayed. Can be either a path
+            string or a pathlib Path object.
+
+    """
+    # Settings
+    dot_size = 20
+    cmap = matplotlib.cm.get_cmap('jet')
+
+    [n_stimuli, n_dim] = z.shape
+
+    n_class = 1
+    if class_id is None:
+        use_legend = False
+
+        class_id = np.ones((n_stimuli))
+        unique_class_list = np.unique(class_id)
+        n_class = 1
+        color_array = cmap((0, 1))
+        color_array = color_array[np.newaxis, 0, :]
+
+        class_legend = ['all']
+    else:
+        use_legend = True
+        unique_class_list = np.unique(class_id)
+        n_class = len(unique_class_list)
+        norm = matplotlib.colors.Normalize(vmin=0., vmax=n_class)
+        color_array = cmap(norm(range(n_class)))
+
+        if classes is not None:
+            class_legend = infer_legend(unique_class_list, classes)
+        else:
+            class_legend = unique_class_list
+
+    # Plot each class separately in order to use legend.
+    for i_class in range(n_class):
+        locs = class_id == unique_class_list[i_class]
+        ax.scatter(
+            z[locs, 0], z[locs, 1], c=color_array[np.newaxis, i_class, :],
+            s=dot_size, label=class_legend[i_class], edgecolors='none')
+
+    if use_legend:
+        ax.legend(bbox_to_anchor=(-.05, 1), loc=1, borderaxespad=0.)
+
+    # ax.set_aspect('equal')
+    # ax.set_xticks([])
+    # ax.set_yticks([])
+
+
+def image_2d(
+        ax, z, class_id=None, classes=None, filepaths=None, image_size=.2,
+        dot_size=10, show_dot=False):
+    """Generate a static scatter plot of the supplied embedding points.
+
+    Arguments:
+        ax: TODO
+        z: A real-valued two-dimensional array representing the embedding.
+            shape = (n_stimuli, n_dim)
+        class_id: (optional) An integer array contianing class IDs
+            that indicate the class membership of each stimulus.
+            shape = (n_stimuli, 1)
+        classes: (optional) A dictionary mapping class IDs to strings.
+        filepaths: TODO should not be optional
+
+    """
+    # Settings
+    cmap = matplotlib.cm.get_cmap('jet')
+
+    [n_stimuli, n_dim] = z.shape
+
+    n_class = 1
+    if class_id is None:
+        use_legend = False
+
+        class_id = np.ones((n_stimuli))
+        unique_class_list = np.unique(class_id)
+        n_class = 1
+        color_array = cmap((0, 1))
+        color_array = color_array[np.newaxis, 0, :]
+
+        class_legend = ['all']
+    else:
+        use_legend = True
+        unique_class_list = np.unique(class_id)
+        n_class = len(unique_class_list)
+        norm = matplotlib.colors.Normalize(vmin=0., vmax=n_class)
+        color_array = cmap(norm(range(n_class)))
+
+        if classes is not None:
+            class_legend = infer_legend(unique_class_list, classes)
+        else:
+            class_legend = unique_class_list
+
+    if show_dot:
+        alpha = 1.0
+    else:
+        use_legend = False
+        alpha = 0.0
+
+    # Plot each class separately in order to use legend.
+    for i_class in range(n_class):
+        locs = class_id == unique_class_list[i_class]
+        ax.scatter(
+            z[locs, 0], z[locs, 1], c=color_array[np.newaxis, i_class, :],
+            s=dot_size, label=class_legend[i_class], edgecolors='none',
+            alpha=alpha
+        )
+
+    # Plot images.
+    for idx, i_file in enumerate(filepaths):
+        img = Image.open(i_file)
+        w, h = img.size
+        ar = w/h
+        dx = ar * image_size
+        dy = image_size
+        x = z[idx, 0]
+        y = z[idx, 1]
+        ax.imshow(img, extent=(x - dx/2, x + dx/2, y - dy/2, y + dy/2))
+
+    if use_legend:
+        lgnd = ax.legend(bbox_to_anchor=(-.05, 1), loc=1, borderaxespad=0.)
+        # Hack to make dots a readable size in legend regardless of size
+        # in plot.
+        for i_handle in lgnd.legendHandles:
+            i_handle._sizes = [30]

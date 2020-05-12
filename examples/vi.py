c26229ce516046665b07235291ee96a7898ad91d
roads <brett.roads@gmail.com>
2020-05-12 13:30:10 +0100

    Start new variational inference example.

--- examples/vi.py
+++ examples/vi.py
@@ -14,16 +14,10 @@
 # limitations under the License.
 # ==============================================================================
 
-"""Example that infers an embedding from two trial configurations.
+"""Example that infers an embedding using variation inference.
 
-Fake data is generated from a ground truth model for two different
-trial configurations: 2-choose-1 and 8-choose-2. This example
-demonstrates how one can use data collected in a variety of formats to
-infer a single embedding.
-
-Example output:
-
-    R^2 Model Comparison:   0.95
+Fake data is generated from a ground truth model for rank 8-choose-2 
+trial configurations.
 
 """
 
@@ -43,31 +37,19 @@ def main():
     # Settings.
     n_stimuli = 25
     n_dim = 3
-    n_restart = 3
+    n_restart = 3  # TODO 20
 
     # Ground truth embedding.
     emb_true = ground_truth(n_stimuli, n_dim)
 
-    # Generate a random docket of trials using two different trial
-    # configurations.
-    # Generate 1500 2-choose-1 trials.
-    n_reference = 2
-    n_select = 1
-    gen_2c1 = psiz.generator.RandomGenerator(
-        n_stimuli, n_reference=n_reference, n_select=n_select
-    )
-    n_trial = 1500
-    docket_2c1 = gen_2c1.generate(n_trial)
-    # Generate 1500 8-choose-2 trials.
+    # Generate a random docket of 8-choose-2 trials.
     n_reference = 8
     n_select = 2
     gen_8c2 = psiz.generator.RandomGenerator(
         n_stimuli, n_reference=n_reference, n_select=n_select
     )
-    n_trial = 1500
-    docket_8c2 = gen_8c2.generate(n_trial)
-    # Merge both sets of trials into a single docket.
-    docket = psiz.trials.stack([docket_2c1, docket_8c2])
+    n_trial = 2000
+    docket = gen_8c2.generate(n_trial)
 
     # Simulate similarity judgments.
     agent = psiz.simulate.Agent(emb_true)
@@ -80,10 +62,15 @@ def main():
     )[0]
     obs_train = obs.subset(train_idx)
     obs_val = obs.subset(val_idx)
+    # Expand dataset for stochastic generative processess. TODO
+    # n_train_sample = 1
+    # obs_train = psiz.trials.stack([obs_train for _ in range(n_train_sample)])
+    n_val_sample = 100
+    obs_val = psiz.trials.stack([obs_val for _ in range(n_val_sample)])
 
     # Use early stopping.
     cb_early = psiz.keras.callbacks.EarlyStoppingRe(
-        'val_nll', patience=10, mode='min', restore_best_weights=True
+        'val_nll', patience=100, mode='min', restore_best_weights=True
     )
     # Visualize using TensorBoard.
     cb_board = psiz.keras.callbacks.TensorBoardRe(
@@ -102,8 +89,24 @@ def main():
     }
 
     # Infer embedding.
-    embedding = tf.keras.layers.Embedding(
-        n_stimuli+1, n_dim, mask_zero=True
+    # embedding = tf.keras.layers.Embedding(
+    #     n_stimuli+1, n_dim, mask_zero=True
+    # )
+    # embeddings_initializer = ed.tensorflow.initializers.TrainableNormal(
+    #     mean_initializer=psiz.keras.initializers.RandomScaleMVN(
+    #         minval=-2., maxval=-1.
+    #     ),
+    #     stddev_initializer=tf.keras.initializers.TruncatedNormal(
+    #         mean=.0001, stddev=0.00001
+    #     ),
+    #     # stddev_initializer=tf.keras.initializers.Constant(value=.0001),
+    # )
+    embedding = ed.layers.EmbeddingReparameterization(
+        n_stimuli+1, output_dim=n_dim, mask_zero=True,
+        # embeddings_initializer=embeddings_initializer,
+        embeddings_regularizer=ed.tensorflow.regularizers.NormalKLDivergence(
+            stddev=.17, scale_factor=0.0
+        )
     )
     similarity = psiz.keras.layers.ExponentialSimilarity()
     rankModel = psiz.models.Rank(
@@ -111,7 +114,7 @@ def main():
     )
     emb_inferred = psiz.models.Proxy(model=rankModel)
     restart_record = emb_inferred.fit(
-        obs_train, validation_data=obs_val, epochs=1000, verbose=1,
+        obs_train, validation_data=obs_val, epochs=1000, verbose=2,
         callbacks=callbacks, n_restart=n_restart, monitor='val_nll',
         compile_kwargs=compile_kwargs
     )
@@ -120,7 +123,9 @@ def main():
     # similarity matrices implied by each model.
     simmat_truth = psiz.utils.pairwise_matrix(emb_true.similarity, emb_true.z)
     simmat_infer = psiz.utils.pairwise_matrix(
-        emb_inferred.similarity, emb_inferred.z
+        emb_inferred.similarity,
+        # emb_inferred.z  # TODO
+        emb_inferred.model.embedding.embeddings_initializer.mean.numpy()[1:, :]  # TODO
     )
     r_squared = psiz.utils.matrix_comparison(
         simmat_truth, simmat_infer, score='r2'

Update simple kernel to fix gradient shape issue.

psiz/keras/layers.py
@@ -670,8 +670,12 @@ class Kernel(tf.keras.layers.Layer):
 
         # Create identity attention weights.
         batch_size = tf.shape(z_0)[0]
+        n_dim = tf.shape(z_0)[1]
+        # NOTE: We must fill in the `dimensionality` dimension in order to
+        # keep shapes compatible between op input and calculated input
+        # gradient.
         # TODO can we always assume 4D?
-        attention = tf.ones([batch_size, 1, 1, 1])
+        attention = tf.ones([batch_size, n_dim, 1, 1])
 
         # Compute distance between query and references.
         dist_qr = self.distance([z_0, z_1, attention])

psiz/models.py
@@ -345,7 +345,7 @@ class Proxy(object):
             else:
                 group_id = group_id.astype(dtype=np.int32)
 
-        attention = self.w[group_id, :]
+        attention = self.w[group_id, :]  # TODO brittle assumption
 
         # Make sure z_q and attention have an appropriate singleton
         # dimensions.
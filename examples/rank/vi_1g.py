6066f834176be409617515bcbe152311be456486
roads <brett.roads@gmail.com>
2020-06-30 11:17:07 +0100

    Update module header format.

--- examples/rank/vi_1g.py
+++ examples/rank/vi_1g.py
@@ -12,8 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
-
+# ============================================================================
 """Example that infers an embedding with an increasing amount of data.
 
 Fake data is generated from a ground truth model assuming one group.

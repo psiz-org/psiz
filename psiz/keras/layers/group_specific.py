4c34ae647b8d9685c06c4becd679a83625b666d9
roads <brett.roads@gmail.com>
2021-02-01 13:10:01 +0000

    Add GroupSpecific layer.

---
+++ psiz/keras/layers/group_specific.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 The PsiZ Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Module for a TensorFlow GroupEmbedding.
+
+Classes:
+    EmbeddingGroup: A layer that manages group-specific subnetworks.
+
+"""
+
+import numpy as np
+import tensorflow as tf
+from psiz.keras.sparse_dispatcher import SparseDispatcher
+
+
+@tf.keras.utils.register_keras_serializable(
+    package='psiz.keras', name='GroupSpecific'
+)
+class GroupSpecific(tf.keras.layers.Layer):
+    """A layer that manages group-specific subnetworks.
+
+    The output shape must be the same for all subnetworks, this
+    includes the sample shape (if any).
+
+    """
+    def __init__(self, subnets=None, group_col=0, **kwargs):
+        """Initialize."""
+        super(GroupSpecific, self).__init__(**kwargs)
+        self.subnets = subnets
+        self.n_subnet = len(subnets)
+        self.group_col = group_col
+
+    def build(self, inputs_shape):
+        """Build."""
+        # Probe first subnet for `sample_shape` information.
+        if hasattr(self.subnets[0], 'n_sample'):
+            self.sample_shape = tf.TensorShape(self.subnets[0].n_sample)
+        else:
+            self.sample_shape = tf.TensorShape(())
+
+        input_shape_less_group = inputs_shape[0]
+        output_shape = self.subnets[0].compute_output_shape(
+            input_shape_less_group
+        )
+
+        # Pre-determine permutation order.
+        # Example with sample_shape.rank=2. This example illustrates how
+        # the permute order and unpermute order are not the same in general.
+        #       [s s b d d ]
+        # start [0 1 2 3 4]
+        # perm  [2 0 1 3 4]
+        # unper [1 2 0 3 4]
+        perm_order = [i for i in range(output_shape.rank)]
+        unperm_order = [i for i in range(output_shape.rank)]
+        current_batch_position = self.sample_shape.rank
+        if current_batch_position > 0:
+            # Perm order.
+            idx = perm_order.pop(current_batch_position)
+            perm_order.insert(0, idx)
+            # Unperm order.
+            idx = unperm_order.pop(0)
+            unperm_order.insert(current_batch_position, idx)
+        self._perm_order = perm_order
+        self._unperm_order = unperm_order
+
+    def call(self, inputs):
+        """Call."""
+        # Split inputs.
+        inputs_less_group = inputs[0]
+        idx_group = inputs[-1][:, self.group_col]
+        idx_group = tf.one_hot(idx_group, self.n_subnet)
+
+        # Handle input reshaping. TODO
+
+        # Run inputs through group-specific dispatcher.
+        dispatcher = SparseDispatcher(self.n_subnet, idx_group)
+        subnet_inputs = dispatcher.dispatch(inputs_less_group)
+        subnet_outputs = []
+        for i in range(self.n_subnet):
+            out, lost_shape = self._pre_combine(
+                self.subnets[i](subnet_inputs[i])
+            )
+            subnet_outputs.append(out)
+        outputs = dispatcher.combine(subnet_outputs, multiply_by_gates=True)
+
+        # Handle reshaping of output.
+        outputs = self._post_combine(outputs, lost_shape)
+
+        return outputs
+
+    def get_config(self):
+        """Return layer configuration."""
+        config = super().get_config()
+        subnets_serial = []
+        for i in range(self.n_subnet):
+            subnets_serial.append(
+                tf.keras.utils.serialize_keras_object(self.subnets[i])
+            )
+        config.update({
+            'subnets': subnets_serial,
+            'group_col': int(self.group_col)
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        subnets_serial = config['subnets']
+        subnets = []
+        for subnet_serial in subnets_serial:
+            subnets.append(
+                tf.keras.layers.deserialize(subnet_serial)
+            )
+        config['subnets'] = subnets
+        return super().from_config(config)
+
+    def subnet(self, subnet_idx):
+        return self.subnets[subnet_idx]
+
+    def _pre_combine(self, x):
+        """Prepare Tensor for combine operation.
+
+        Must first move any `sample_shape` to the right of batch
+        dimension. All non-batch dimensions must be flattened together
+        to create a final 2D Tensor.
+
+        Arguments:
+            x: Data Tensor.
+
+        Returns:
+            x_2d: Transformed data Tensor.
+            lost_shape: The original (i.e., lost) shape of the non-batch
+                dimensions.
+
+        """
+        # Start by moving sample dimensions.
+        x = tf.transpose(x, self._perm_order)
+
+        # Now flatten non-batch dimensions.
+        x_shape = tf.shape(x)
+        lost_shape = x_shape[1:]
+        x = tf.reshape(
+            x, [x_shape[0], tf.reduce_prod(lost_shape)]
+        )
+        return x, lost_shape
+
+    def _post_combine(self, x, lost_shape):
+        """Handle post-combine operations."""
+        batch_size = tf.expand_dims(tf.shape(x)[0], axis=0)
+        # Unflatten non-batch dimensions.
+        desired_shape = tf.concat(
+            (batch_size, lost_shape), axis=0
+        )
+        x = tf.reshape(x, desired_shape)
+
+        # Move sample dimensions back to left-hand side.
+        x = tf.transpose(x, self._unperm_order)
+        return x
---
+++ tests/keras/layers/test_group_specific.py
@@ -0,0 +1,385 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 The PsiZ Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Test GroupSpecific."""
+
+import pytest
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Embedding
+import tensorflow_probability as tfp
+
+from psiz.keras.layers import EmbeddingNormalDiag
+from psiz.keras.layers.group_specific import GroupSpecific
+
+
+@pytest.fixture
+def subnets_determ():
+    """A list of subnets"""
+    base_array = np.array([
+        [0.0, 0.1, 0.2],
+        [1.0, 1.1, 1.2],
+        [2.0, 2.1, 2.2],
+        [3.0, 3.1, 3.2],
+        [4.0, 4.1, 4.2],
+        [5.0, 5.1, 5.2],
+        [6.0, 6.1, 6.2],
+        [7.0, 7.1, 7.2],
+        [8.0, 8.1, 8.2],
+        [9.0, 9.1, 9.2],
+        [10.0, 10.1, 10.2],
+        [11.0, 11.1, 11.2],
+    ], dtype=np.float32)
+
+    emb_0 = Embedding(
+        input_dim=12, output_dim=3,
+        embeddings_initializer=tf.keras.initializers.Constant(base_array)
+    )
+    emb_1 = Embedding(
+        input_dim=12, output_dim=3,
+        embeddings_initializer=tf.keras.initializers.Constant(base_array + 100)
+    )
+    emb_2 = Embedding(
+        input_dim=12, output_dim=3,
+        embeddings_initializer=tf.keras.initializers.Constant(base_array + 200)
+    )
+    subnets = [emb_0, emb_1, emb_2]
+    return subnets
+
+
+@pytest.fixture
+def subnets_dist_rank0():
+    """A list of subnets"""
+    # Settings.
+    prior_scale = .0000000001
+    n_stimuli = 12
+    n_dim = 3
+    n_sample = ()
+
+    base_array = np.array([
+        [0.0, 0.1, 0.2],
+        [1.0, 1.1, 1.2],
+        [2.0, 2.1, 2.2],
+        [3.0, 3.1, 3.2],
+        [4.0, 4.1, 4.2],
+        [5.0, 5.1, 5.2],
+        [6.0, 6.1, 6.2],
+        [7.0, 7.1, 7.2],
+        [8.0, 8.1, 8.2],
+        [9.0, 9.1, 9.2],
+        [10.0, 10.1, 10.2],
+        [11.0, 11.1, 11.2],
+    ], dtype=np.float32)
+
+    emb_0 = EmbeddingNormalDiag(
+        n_stimuli, n_dim, mask_zero=True,
+        loc_initializer=tf.keras.initializers.Constant(
+            base_array
+        ),
+        scale_initializer=tf.keras.initializers.Constant(
+            tfp.math.softplus_inverse(prior_scale).numpy()
+        )
+    )
+    emb_0.n_sample = n_sample
+
+    emb_1 = EmbeddingNormalDiag(
+        n_stimuli, n_dim, mask_zero=True,
+        loc_initializer=tf.keras.initializers.Constant(
+            base_array + 100
+        ),
+        scale_initializer=tf.keras.initializers.Constant(
+            tfp.math.softplus_inverse(prior_scale).numpy()
+        )
+    )
+    emb_1.n_sample = n_sample
+
+    emb_2 = EmbeddingNormalDiag(
+        n_stimuli, n_dim, mask_zero=True,
+        loc_initializer=tf.keras.initializers.Constant(
+            base_array + 200
+        ),
+        scale_initializer=tf.keras.initializers.Constant(
+            tfp.math.softplus_inverse(prior_scale).numpy()
+        )
+    )
+    emb_2.n_sample = n_sample
+
+    subnets = [emb_0, emb_1, emb_2]
+    return subnets
+
+
+@pytest.fixture
+def subnets_dist_rank1():
+    """A list of subnets"""
+    # Settings.
+    prior_scale = .0000000001
+    n_stimuli = 12
+    n_dim = 3
+    n_sample = 11
+
+    base_array = np.array([
+        [0.0, 0.1, 0.2],
+        [1.0, 1.1, 1.2],
+        [2.0, 2.1, 2.2],
+        [3.0, 3.1, 3.2],
+        [4.0, 4.1, 4.2],
+        [5.0, 5.1, 5.2],
+        [6.0, 6.1, 6.2],
+        [7.0, 7.1, 7.2],
+        [8.0, 8.1, 8.2],
+        [9.0, 9.1, 9.2],
+        [10.0, 10.1, 10.2],
+        [11.0, 11.1, 11.2],
+    ], dtype=np.float32)
+
+    emb_0 = EmbeddingNormalDiag(
+        n_stimuli, n_dim, mask_zero=True,
+        loc_initializer=tf.keras.initializers.Constant(
+            base_array
+        ),
+        scale_initializer=tf.keras.initializers.Constant(
+            tfp.math.softplus_inverse(prior_scale).numpy()
+        )
+    )
+    emb_0.n_sample = n_sample
+
+    emb_1 = EmbeddingNormalDiag(
+        n_stimuli, n_dim, mask_zero=True,
+        loc_initializer=tf.keras.initializers.Constant(
+            base_array + 100
+        ),
+        scale_initializer=tf.keras.initializers.Constant(
+            tfp.math.softplus_inverse(prior_scale).numpy()
+        )
+    )
+    emb_1.n_sample = n_sample
+
+    emb_2 = EmbeddingNormalDiag(
+        n_stimuli, n_dim, mask_zero=True,
+        loc_initializer=tf.keras.initializers.Constant(
+            base_array + 200
+        ),
+        scale_initializer=tf.keras.initializers.Constant(
+            tfp.math.softplus_inverse(prior_scale).numpy()
+        )
+    )
+    emb_2.n_sample = n_sample
+
+    subnets = [emb_0, emb_1, emb_2]
+    return subnets
+
+
+@pytest.fixture
+def group_v0():
+    """A minibatch of group indices."""
+    # Create a simple batch (batch_size=5).
+    group = tf.constant(
+        np.array(
+            [
+                [0, 0],
+                [1, 0],
+                [2, 0],
+                [1, 1],
+                [2, 1]
+            ], dtype=np.int32
+        )
+    )
+    return group
+
+
+@pytest.fixture
+def inputs_v0():
+    """A minibatch of non-gate inupts."""
+    # Create a simple batch (batch_size=5).
+    inputs = tf.constant(
+        np.array(
+            [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [9, 10, 11],
+                [9, 10, 11]
+            ], dtype=np.int32
+        )
+    )
+    return inputs
+
+
+@pytest.fixture
+def inputs_v1():
+    """A minibatch of non-gate inupts."""
+    # Create a simple batch (batch_size=5).
+    inputs = tf.constant(
+        np.array(
+            [
+                [[0, 1, 2], [3, 4, 5]],
+                [[0, 1, 2], [3, 4, 5]],
+                [[0, 1, 2], [3, 4, 5]],
+                [[6, 7, 8], [9, 10, 11]],
+                [[6, 7, 8], [9, 10, 11]]
+            ], dtype=np.int32
+        )
+    )
+    return inputs
+
+
+def test_init(subnets_determ):
+    group_layer = GroupSpecific(subnets=subnets_determ)
+
+
+def test_call_determ_2d_input(subnets_determ, inputs_v0, group_v0):
+    """Test call that does not require an internal reshape."""
+    group_layer = GroupSpecific(subnets=subnets_determ)
+
+    outputs = group_layer([inputs_v0, group_v0])
+
+    desired_outputs = np.array([
+        [[0.0, 0.1, 0.2], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2]],
+        [[103.0, 103.1, 103.2], [104.0, 104.1, 104.2], [105.0, 105.1, 105.2]],
+        [[206.0, 206.1, 206.2], [207.0, 207.1, 207.2], [208.0, 208.1, 208.2]],
+        [[109.0, 109.1, 109.2], [110.0, 110.1, 110.2], [111.0, 111.1, 111.2]],
+        [[209.0, 209.1, 209.2], [210.0, 210.1, 210.2], [211.0, 211.1, 211.2]],
+    ], dtype=np.float32)
+    np.testing.assert_array_almost_equal(outputs.numpy(), desired_outputs)
+
+
+def test_call_determ_3d_input(subnets_determ, inputs_v1, group_v0):
+    """Test call with data inputs larger than 2D."""
+    group_layer = GroupSpecific(subnets=subnets_determ)
+
+    outputs = group_layer([inputs_v1, group_v0])
+
+    desired_outputs = np.array([
+        [
+            [[0.0, 0.1, 0.2], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2]],
+            [[3.0, 3.1, 3.2], [4.0, 4.1, 4.2], [5.0, 5.1, 5.2]]
+        ],
+        [
+            [
+                [100.0, 100.1, 100.2],
+                [101.0, 101.1, 101.2],
+                [102.0, 102.1, 102.2]
+            ],
+            [
+                [103.0, 103.1, 103.2],
+                [104.0, 104.1, 104.2],
+                [105.0, 105.1, 105.2]]
+        ],
+        [
+            [
+                [200.0, 200.1, 200.2],
+                [201.0, 201.1, 201.2],
+                [202.0, 202.1, 202.2]
+            ],
+            [
+                [203.0, 203.1, 203.2],
+                [204.0, 204.1, 204.2],
+                [205.0, 205.1, 205.2]
+            ]
+        ],
+        [
+            [
+                [106.0, 106.1, 106.2],
+                [107.0, 107.1, 107.2],
+                [108.0, 108.1, 108.2]
+            ],
+            [
+                [109.0, 109.1, 109.2],
+                [110.0, 110.1, 110.2],
+                [111.0, 111.1, 111.2]
+            ]
+        ],
+        [
+            [
+                [206.0, 206.1, 206.2],
+                [207.0, 207.1, 207.2],
+                [208.0, 208.1, 208.2]
+            ],
+            [
+                [209.0, 209.1, 209.2],
+                [210.0, 210.1, 210.2],
+                [211.0, 211.1, 211.2]
+            ]
+        ]
+    ], dtype=np.float32)
+    np.testing.assert_array_almost_equal(outputs.numpy(), desired_outputs)
+
+
+def test_call_dist_2d_input_rank0(subnets_dist_rank0, inputs_v0, group_v0):
+    """Test call that does not require an internal reshape."""
+    group_layer = GroupSpecific(subnets=subnets_dist_rank0)
+
+    outputs = group_layer([inputs_v0, group_v0])
+
+    desired_outputs = np.array([
+        [[0.0, 0.1, 0.2], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2]],
+        [[103.0, 103.1, 103.2], [104.0, 104.1, 104.2], [105.0, 105.1, 105.2]],
+        [[206.0, 206.1, 206.2], [207.0, 207.1, 207.2], [208.0, 208.1, 208.2]],
+        [[109.0, 109.1, 109.2], [110.0, 110.1, 110.2], [111.0, 111.1, 111.2]],
+        [[209.0, 209.1, 209.2], [210.0, 210.1, 210.2], [211.0, 211.1, 211.2]],
+    ], dtype=np.float32)
+    np.testing.assert_array_almost_equal(
+        outputs.numpy(), desired_outputs, decimal=1
+    )
+
+
+def test_call_dist_2d_input_rank1(subnets_dist_rank1, inputs_v0, group_v0):
+    """Test call that does not require an internal reshape."""
+    group_layer = GroupSpecific(subnets=subnets_dist_rank1)
+
+    outputs = group_layer([inputs_v0, group_v0])
+    outputs_avg = tf.reduce_mean(outputs, axis=0)
+
+    desired_outputs = np.array([
+        [[0.0, 0.1, 0.2], [1.0, 1.1, 1.2], [2.0, 2.1, 2.2]],
+        [[103.0, 103.1, 103.2], [104.0, 104.1, 104.2], [105.0, 105.1, 105.2]],
+        [[206.0, 206.1, 206.2], [207.0, 207.1, 207.2], [208.0, 208.1, 208.2]],
+        [[109.0, 109.1, 109.2], [110.0, 110.1, 110.2], [111.0, 111.1, 111.2]],
+        [[209.0, 209.1, 209.2], [210.0, 210.1, 210.2], [211.0, 211.1, 211.2]],
+    ], dtype=np.float32)
+    np.testing.assert_array_almost_equal(
+        outputs_avg.numpy(), desired_outputs, decimal=1
+    )
+
+
+def test_serialization(subnets_dist_rank1, inputs_v0, group_v0):
+    # Build group-specific layer.
+    group_layer = GroupSpecific(subnets=subnets_dist_rank1)
+
+    # Get configuration.
+    config = group_layer.get_config()
+
+    # Reconstruct layer from configuration.
+    group_layer_recon = GroupSpecific.from_config(config)
+
+    # Must set `n_sample` since this is not saved.
+    for subnet in group_layer_recon.subnets:
+        subnet.n_sample = 11
+
+    # Assert reconstructed attributes are the same as original.
+    assert group_layer.n_subnet == group_layer_recon.n_subnet
+    assert group_layer.group_col == group_layer_recon.group_col
+
+    outputs = group_layer([inputs_v0, group_v0])
+    outputs_avg = tf.reduce_mean(outputs, axis=0)
+
+    outputs_recon = group_layer_recon([inputs_v0, group_v0])
+    outputs_avg_recon = tf.reduce_mean(outputs_recon, axis=0)
+
+    # Assert calls are roughly equal (given constant initializer).
+    np.testing.assert_array_almost_equal(
+        outputs_avg.numpy(), outputs_avg_recon.numpy(), decimal=1
+    )

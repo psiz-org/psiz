d1435988fb5aac6c9a25a45bc55cd90c9fb3798f
roads <brett.roads@gmail.com>
2021-02-02 10:01:26 +0000

    Update docstrings

--- psiz/keras/layers/group_specific.py
+++ psiz/keras/layers/group_specific.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Module for a TensorFlow GroupEmbedding.
+"""Module for a TensorFlow GroupSpecific.
 
 Classes:
     EmbeddingGroup: A layer that manages group-specific subnetworks.

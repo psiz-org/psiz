b105d92f880b2bc0c6aa3169d96aa86f913fce6d
roads <brett.roads@gmail.com>
2021-05-21 18:55:03 +0100

    Update TF version.
    Patches security vulnerability associated with TF 2.4.x.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     package_dir={'': 'src'},
     python_requires='>=3.7, <3.10',
     install_requires=[
-        'tensorflow>=2.4, <2.6', 'tensorflow-probability==0.11.0', 'pandas',
+        'tensorflow>=2.4.2, <2.6', 'tensorflow-probability==0.11.0', 'pandas',
         'scikit-learn', 'matplotlib', 'pillow', 'imageio'
     ],
     project_urls={

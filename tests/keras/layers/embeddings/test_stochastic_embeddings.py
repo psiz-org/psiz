a7a5c61f54c96a7ea6e523c3ecdceabd7ceca015
roads <brett.roads@gmail.com>
2021-08-11 20:20:16 +0100

    Update docstring

--- tests/keras/layers/embeddings/test_stochastic_embeddings.py
+++ tests/keras/layers/embeddings/test_stochastic_embeddings.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Module for testing embeddings."""
+"""Module for testing stochastic embeddings."""
 
 import pytest
 

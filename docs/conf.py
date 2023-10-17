56d2438abadd2d0b2ff0c2a5d3a1f5d74068f30c
roads <brett.roads@gmail.com>
2023-10-17 23:23:19 +0100

    Bump psiz package release/version in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+
+sys.path.insert(0, os.path.abspath("../src"))
 
 # Need to import as get_version since version is a reserved variable name in
 # sphinx.
@@ -24,14 +25,14 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'psiz'
-copyright = '2021, The PsiZ Authors'
-author = 'Brett D. Roads'
+project = "psiz"
+copyright = "2021, The PsiZ Authors"
+author = "Brett D. Roads"
 
 # TODO remove release/version stuff?
 # The full version, including alpha/beta/rc tags
-release = '0.10.0'
-version = '0.10'
+release = "0.11.0"
+version = "0.11"
 # release = get_version("psiz")
 # version = '.'.join(release.split('.')[:3])
 
@@ -41,22 +42,22 @@ version = '0.10'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.autodoc',  # automatic documentation from docstrings
-    'sphinx.ext.napoleon',  # autodoc parsing of Google style docstrings
-    'sphinx.ext.imgmath',  # Render math equations.
-    'sphinx_last_updated_by_git',  # Infer last updated date via git.
-    'myst_nb',  # Parsing Jupyter notebooks.
-    'sphinxcontrib.bibtex',  # bibtex bibliography
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",  # automatic documentation from docstrings
+    "sphinx.ext.napoleon",  # autodoc parsing of Google style docstrings
+    "sphinx.ext.imgmath",  # Render math equations.
+    "sphinx_last_updated_by_git",  # Infer last updated date via git.
+    "myst_nb",  # Parsing Jupyter notebooks.
+    "sphinxcontrib.bibtex",  # bibtex bibliography
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -64,17 +65,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_favicon = 'img/favicon.ico'
-html_logo = 'img/full_logo_300.png'
+html_favicon = "img/favicon.ico"
+html_logo = "img/full_logo_300.png"
 html_theme_options = {
-    'logo_only': True,
+    "logo_only": True,
 }
 numfig = True
 
@@ -84,8 +85,8 @@ myst_enable_extensions = [
 ]
 
 # bibtex settings.
-bibtex_bibfiles = ['src/refs.bib']
-bibtex_reference_style = 'author_year'
+bibtex_bibfiles = ["src/refs.bib"]
+bibtex_reference_style = "author_year"
 
 # Notebook execution settings.
 nb_execution_mode = "off"  # Do not execute cells.
